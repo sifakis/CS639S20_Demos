c7de60817216d525b95c757750240903c91dfd34
Eftychios Sifakis <eftychios.sifakis.-nd@disneyresearch.com>
2020-02-13 13:39:10 -0600

    include fix and supplemental cases

--- LaplaceSolver/LaplaceSolver_0_1/Utilities.cpp
+++ LaplaceSolver/LaplaceSolver_0_1/Utilities.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
+#include <stdexcept>
 
 void Clear(float (&x)[XDIM][YDIM][ZDIM])
 {
@@ -33,7 +34,6 @@ void WriteAsImage(const std::string& filenamePrefix, const float (&x)[XDIM][YDIM
     std::ostringstream filename;
     filename << filenamePrefix << "." << std::setfill('0') << std::setw(4) << count << ".pgm";
     
-    std::cout << "here" << std::endl;
     std::ofstream output(filename.str());
 
     output << "P2" << std::endl;
@@ -45,11 +45,31 @@ void WriteAsImage(const std::string& filenamePrefix, const float (&x)[XDIM][YDIM
     }
     output << "255" << std::endl;
 
-    for (int j = 0; j < YDIM; j++){
-        for (int k = 0; k < ZDIM; k++)
-            output << (int)(x[slice][j][k]*255.0) << " ";
-        output << std::endl;
+    switch(axis){
+        
+        case 0:
+            for (int j = 0; j < YDIM; j++){
+                for (int k = 0; k < ZDIM; k++)
+                    output << (int)(x[slice][j][k]*255.0) << " ";
+                output << std::endl;
+            }
+            break;
+        case 1:
+            for (int i = 0; i < XDIM; i++){
+                for (int k = 0; k < ZDIM; k++)
+                    output << (int)(x[i][slice][k]*255.0) << " ";
+                output << std::endl;
+            }
+            break;
+        case 2:
+            for (int i = 0; i < XDIM; i++){
+                for (int j = 0; j < YDIM; j++)
+                    output << (int)(x[i][j][slice]*255.0) << " ";
+                output << std::endl;
+            }
+            break;
+        default: throw std::logic_error("Invalid axis in WriteAsImage()");
     }
-            
+    
     output.close();
 }
