98bde6f94ce1a4352968ea783a2611c29578b171
Eftychios Sifakis <sifakis@github.com>
2020-03-23 14:51:45 -0500

    implementation using pre-transposition

---
+++ DenseAlgebra/GEMM_Test_0_4/MatMatMultiply.cpp
@@ -0,0 +1,52 @@
+#include "MatMatMultiply.h"
+#include "mkl.h"
+
+void MatTranspose(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    float (&AT)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    mkl_somatcopy(
+        'R',         // Matrix A is in row-major format
+        'T',         // We are performing a transposition operation
+        MATRIX_SIZE, // Dimensions of matrix -- rows ...
+        MATRIX_SIZE, // ... and columns
+        1.,          // No scaling
+        &A[0][0],    // Input matrix
+        MATRIX_SIZE, // Leading dimension (here, just the matrix dimension)
+        &AT[0][0],   // Output matrix
+        MATRIX_SIZE  // Leading dimension
+    );
+}
+
+// Matrix A is presumed to be row-major, matrix B presumed to be column-major
+void MatMatTransposeMultiply(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+#pragma omp parallel for
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    for (int j = 0; j < MATRIX_SIZE; j++) {
+        C[i][j] = 0.;
+        for (int k = 0; k < MATRIX_SIZE; k++)
+            C[i][j] += A[i][k] * B[j][k];
+    }
+}
+
+void MatMatMultiplyReference(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    cblas_sgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        1.,
+        &A[0][0],
+        MATRIX_SIZE,
+        &B[0][0],
+        MATRIX_SIZE,
+        0.,
+        &C[0][0],
+        MATRIX_SIZE
+    );
+}
