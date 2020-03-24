629e45de07c76f9331eb2fda9a231b23297b42f9
Eftychios Sifakis <sifakis@github.com>
2020-03-23 19:39:45 -0500

    local buffer for blocking

---
+++ DenseAlgebra/GEMM_Test_0_7/MatMatMultiply.cpp
@@ -0,0 +1,91 @@
+#include "MatMatMultiply.h"
+#include "mkl.h"
+
+void MatTranspose(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    float (&AT)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    mkl_somatcopy(
+        'R',         // Matrix A is in row-major format
+        'T',         // We are performing a transposition operation
+        MATRIX_SIZE, // Dimensions of matrix -- rows ...
+        MATRIX_SIZE, // ... and columns
+        1.,          // No scaling
+        &A[0][0],    // Input matrix
+        MATRIX_SIZE, // Leading dimension (here, just the matrix dimension)
+        &AT[0][0],   // Output matrix
+        MATRIX_SIZE  // Leading dimension
+    );
+}
+
+// Matrix A is presumed to be row-major, matrix B presumed to be column-major
+void MatMatTransposeMultiply(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    static constexpr int NBLOCKS = MATRIX_SIZE / BLOCK_SIZE;
+
+    using blocked_matrix_t = float (&) [NBLOCKS][BLOCK_SIZE][NBLOCKS][BLOCK_SIZE];
+    using const_blocked_matrix_t = const float (&) [NBLOCKS][BLOCK_SIZE][NBLOCKS][BLOCK_SIZE];
+
+    auto blockA = reinterpret_cast<const_blocked_matrix_t>(A[0][0]);
+    auto blockB = reinterpret_cast<const_blocked_matrix_t>(B[0][0]);
+    auto blockC = reinterpret_cast<blocked_matrix_t>(C[0][0]);
+
+#pragma omp parallel for
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    for (int j = 0; j < MATRIX_SIZE; j++)
+        C[i][j] = 0.;    
+
+#pragma omp parallel for
+    for (int bi = 0; bi < NBLOCKS; bi++)
+    for (int bj = 0; bj < NBLOCKS; bj++)
+        for (int bk = 0; bk < NBLOCKS; bk++) { 
+
+            float localA[BLOCK_SIZE][BLOCK_SIZE];
+            float localB[BLOCK_SIZE][BLOCK_SIZE];
+            float localC[BLOCK_SIZE][BLOCK_SIZE];
+
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            for (int j = 0; j < BLOCK_SIZE; j++) {
+                localA[i][j] = blockA[bi][i][bk][j];
+                localB[i][j] = blockB[bj][i][bk][j];
+                    
+            }
+
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            for (int j = 0; j < BLOCK_SIZE; j++) {
+                localC[i][j] = 0.;
+#pragma omp simd
+                for (int k = 0; k < BLOCK_SIZE; k++)
+                    localC[i][j] += localA[i][k] * localB[j][k];
+                //blockC[bi][i][bj][j] += blockA[bi][i][bk][k]*blockB[bj][j][bk][k];
+                    // blockC[bi][i][bj][j] += blockA[bi][i][bk][k]*blockB[bj][j][bk][k];
+            }
+
+            for (int i = 0; i < BLOCK_SIZE; i++)
+            for (int j = 0; j < BLOCK_SIZE; j++)
+                
+                blockC[bi][i][bj][j] += localC[i][j];
+            
+        }
+}
+
+void MatMatMultiplyReference(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    cblas_sgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        1.,
+        &A[0][0],
+        MATRIX_SIZE,
+        &B[0][0],
+        MATRIX_SIZE,
+        0.,
+        &C[0][0],
+        MATRIX_SIZE
+    );
+}
