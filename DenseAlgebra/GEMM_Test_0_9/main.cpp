e7ec1e3cc47a8985c86cc34f6184983be5f2d7d0
Eftychios Sifakis <sifakis@github.com>
2020-03-30 01:15:29 -0500

    reverting to non-transposed, blocked format, with thread-local temp results

---
+++ DenseAlgebra/GEMM_Test_0_9/main.cpp
@@ -0,0 +1,48 @@
+#include "MatMatMultiply.h"
+#include "Timer.h"
+#include "Utilities.h"
+
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char *argv[])
+{
+    float *Araw = static_cast<float*>( AlignedAllocate( MATRIX_SIZE * MATRIX_SIZE * sizeof(float), 64 ) );
+    float *Braw = static_cast<float*>( AlignedAllocate( MATRIX_SIZE * MATRIX_SIZE * sizeof(float), 64 ) );
+    float *Craw = static_cast<float*>( AlignedAllocate( MATRIX_SIZE * MATRIX_SIZE * sizeof(float), 64 ) );
+    float *referenceCraw = static_cast<float*>( AlignedAllocate( MATRIX_SIZE * MATRIX_SIZE * sizeof(float), 64 ) );
+
+    using matrix_t = float (&) [MATRIX_SIZE][MATRIX_SIZE];
+
+    matrix_t A = reinterpret_cast<matrix_t>(*Araw);
+    matrix_t B = reinterpret_cast<matrix_t>(*Braw);
+    matrix_t C = reinterpret_cast<matrix_t>(*Craw);
+    matrix_t referenceC = reinterpret_cast<matrix_t>(*referenceCraw);
+
+    InitializeMatrices(A, B);
+    Timer timer;
+
+    // Correctness test
+    std::cout << "Running candidate kernel for correctness test ... " << std::flush;
+    timer.Start();
+    MatMatMultiply(A, B, C);
+    timer.Stop("Elapsed time : ");
+    
+    std::cout << "Running reference kernel for correctness test ... " << std::flush;
+    timer.Start();
+    MatMatMultiplyReference(A, B, referenceC);
+    timer.Stop("Elapsed time : ");
+
+    float discrepancy = MatrixMaxDifference(C, referenceC);
+    std::cout << "Discrepancy between two methods : " << discrepancy << std::endl;
+    
+    for(int test = 1; test <= 20; test++)
+    {
+        std::cout << "Running kernel for performance run #" << std::setw(2) << test << " ... ";
+        timer.Start();
+        MatMatMultiply(A, B, C);
+        timer.Stop("Elapsed time : ");
+    }
+    
+    return 0;
+}
