9a3af06bc7ff89bf9c8fafc34f85551165664ce7
Eftychios Sifakis <sifakis@github.com>
2020-04-07 01:30:06 -0500

    intrinsics example (avx2)

---
+++ DenseAlgebra/GEMM_Test_1_0_avx2/MatMatMultiply.cpp
@@ -0,0 +1,68 @@
+#include "MatMatMultiply.h"
+#include "MatMatMultiplyBlockHelper.h"
+#include "mkl.h"
+
+alignas(64) float localA[BLOCK_SIZE][BLOCK_SIZE];
+alignas(64) float localB[BLOCK_SIZE][BLOCK_SIZE];
+alignas(64) float localC[BLOCK_SIZE][BLOCK_SIZE];
+
+#pragma omp threadprivate(localA, localB, localC)
+
+void MatMatMultiply(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    static constexpr int NBLOCKS = MATRIX_SIZE / BLOCK_SIZE;
+
+    using blocked_matrix_t = float (&) [NBLOCKS][BLOCK_SIZE][NBLOCKS][BLOCK_SIZE];
+    using const_blocked_matrix_t = const float (&) [NBLOCKS][BLOCK_SIZE][NBLOCKS][BLOCK_SIZE];
+
+    auto blockA = reinterpret_cast<const_blocked_matrix_t>(A[0][0]);
+    auto blockB = reinterpret_cast<const_blocked_matrix_t>(B[0][0]);
+    auto blockC = reinterpret_cast<blocked_matrix_t>(C[0][0]);
+
+#pragma omp parallel for
+    for (int bi = 0; bi < NBLOCKS; bi++)
+    for (int bj = 0; bj < NBLOCKS; bj++) {
+        
+        for (int ii = 0; ii < BLOCK_SIZE; ii++)
+            for (int jj = 0; jj < BLOCK_SIZE; jj++) {
+                localC[ii][jj] = 0.;
+            }
+
+        for (int bk = 0; bk < NBLOCKS; bk++) { 
+
+            for (int ii = 0; ii < BLOCK_SIZE; ii++)
+            for (int jj = 0; jj < BLOCK_SIZE; jj++) {
+                localA[ii][jj] = blockA[bi][ii][bk][jj];
+                localB[ii][jj] = blockB[bk][ii][bj][jj];
+            }
+
+            MatMatMultiplyBlockHelper(localA, localB, localC);
+        }
+
+        for (int ii = 0; ii < BLOCK_SIZE; ii++)
+        for (int jj = 0; jj < BLOCK_SIZE; jj++)                
+            blockC[bi][ii][bj][jj] = localC[ii][jj];
+    }
+}
+
+void MatMatMultiplyReference(const float (&A)[MATRIX_SIZE][MATRIX_SIZE],
+    const float (&B)[MATRIX_SIZE][MATRIX_SIZE], float (&C)[MATRIX_SIZE][MATRIX_SIZE])
+{
+    cblas_sgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        MATRIX_SIZE,
+        1.,
+        &A[0][0],
+        MATRIX_SIZE,
+        &B[0][0],
+        MATRIX_SIZE,
+        0.,
+        &C[0][0],
+        MATRIX_SIZE
+    );
+}
