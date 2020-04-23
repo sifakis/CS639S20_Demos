942776fbb084c7659c3f391240e9594efad6d06a
Eftychios Sifakis <eftychios.sifakis.-nd@disneyresearch.com>
2020-04-23 13:41:52 -0500

    minor fix for some compilers

--- Prefetching/SparseSaxpy_0_1/PointwiseOps.cpp
+++ Prefetching/SparseSaxpy_0_1/PointwiseOps.cpp
@@ -8,10 +8,10 @@ void SparseSaxpy(std::vector<int>& blockOffsets, const float *x, float *y, const
     
 #pragma omp parallel for
     for (int b = 0; b < blockOffsets.size(); b++) {
-        _mm_prefetch ( &x[blockOffsets[b+L2_PREFETCH_DISTANCE]], _MM_HINT_T2 );
-        _mm_prefetch ( &x[blockOffsets[b+L1_PREFETCH_DISTANCE]], _MM_HINT_T1 );
-        _mm_prefetch ( &y[blockOffsets[b+L2_PREFETCH_DISTANCE]], _MM_HINT_T2 );
-        _mm_prefetch ( &y[blockOffsets[b+L1_PREFETCH_DISTANCE]], _MM_HINT_T1 );
+        _mm_prefetch ( reinterpret_cast<const char *>(&x[blockOffsets[b+L2_PREFETCH_DISTANCE]]), _MM_HINT_T2 );
+        _mm_prefetch ( reinterpret_cast<const char *>(&x[blockOffsets[b+L1_PREFETCH_DISTANCE]]), _MM_HINT_T1 );
+        _mm_prefetch ( reinterpret_cast<const char *>(&y[blockOffsets[b+L2_PREFETCH_DISTANCE]]), _MM_HINT_T2 );
+        _mm_prefetch ( reinterpret_cast<const char *>(&y[blockOffsets[b+L1_PREFETCH_DISTANCE]]), _MM_HINT_T1 );
 #pragma omp simd
         for (int i = 0; i < BLOCK_SIZE; i++)
             y[blockOffsets[b]+i] += scale * x[blockOffsets[b]+i];
